748dfdb4f800d749f707b34729a0f67f7f8ec836
arthurbz <arthurbziero@gmail.com>
2021-01-09 19:14:56 -0300

    handling exceptions
    Handling file excpetions and raising errors. Also added an output name to the .jpg and a verbose parameter.

--- dataset_generator.py
+++ dataset_generator.py
@@ -2,28 +2,32 @@ from pathlib import Path
 import cv2
 
 
-def generate(folder, video_path, steps, step_type='frame'):
+def generate(folder, video_path, steps, step_type, output_name='frame', verbose=1):
+    options = {'frame': generate_by_frame, 'seconds': generate_by_seconds}
+
     if(step_type != 'frame' and step_type != 'seconds'):
-        return 'Step type has to be frame or seconds.'
-    files_path = check_folder(folder)
-    print(files_path)
-    options = {'frame': generate_by_frame,
-               'seconds': generate_by_seconds}
-    options[step_type](files_path, video_path, steps)
+        raise ValueError('Step has to be frame or seconds.')
+
+    try:
+        files_path = check_folder(folder)
+    except FileNotFoundError or OSError:
+        raise
+
+    if verbose: print('Destiny folder:', files_path)
+    options[step_type](files_path, video_path, output_name, steps, verbose)
 
 
-def generate_by_seconds(folder, video_path, steps):
+def generate_by_seconds(folder, video_path, output_name, steps, verbose):
     video = cv2.VideoCapture(video_path)
     fps = video.get(cv2.CAP_PROP_FPS)
-
     currentframe = 0
 
     while (True):
         ret, frame = video.read()
         if ret:
             if (currentframe / fps) % steps == 0:
-                name = folder + str(currentframe) + '.jpg'
-                print('Creating...' + name)
+                name = folder + output_name + str(currentframe) + '.jpg'
+                if verbose: print('Creating...' + name)
                 cv2.imwrite(name, frame)
             currentframe += 1
         else:
@@ -32,17 +36,16 @@ def generate_by_seconds(folder, video_path, steps):
     cv2.destroyAllWindows()
 
 
-def generate_by_frame(folder, video_path, steps):
+def generate_by_frame(folder, video_path, output_name, steps, verbose):
     video = cv2.VideoCapture(video_path)
     currentframe = 0
 
     while (True):
         ret, frame = video.read()
-        print(currentframe)
         if ret:
             if currentframe % steps == 0:
-                name = folder + str(currentframe) + '.jpg'
-                print('Creating...' + name)
+                name = folder + output_name + str(currentframe) + '.jpg'
+                if verbose: print('Creating...' + name)
                 cv2.imwrite(name, frame)
             currentframe += 1
         else:
@@ -52,5 +55,8 @@ def generate_by_frame(folder, video_path, steps):
 
 
 def check_folder(folder):
-    Path(folder).mkdir(parents=True, exist_ok=True)
-    return str(Path(folder).absolute()) + '/'
+    try:
+        Path(folder).mkdir(parents=True, exist_ok=True)
+        return str(Path(folder).absolute()) + '/'
+    except FileNotFoundError or OSError:
+        raise
--- main.py
+++
@@ -1,4 +0,0 @@
-import dataset_generator
-
-dataset_generator.generate('/media/badass/dados/datatest/test2',
-                           '/home/badass/Downloads/kimi.mp4', 30, 'seconds')
